21f4d18ee6b0da68219871bb8209146097daf598
olumayor99 <olumayoor99@gmail.com>
2021-01-01 14:16:29 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: 'https://amazona.webacademy.pro/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: 'https://amazona.webacademy.pro/images/p2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 6,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: 'https://amazona.webacademy.pro/images/p3.jpg',
+            price: 150,
+            brand: 'Lacoste',
+            rating: 4.6,
+            numReviews: 14,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: 'https://amazona.webacademy.pro/images/p4.jpg',
+            price: 162,
+            brand: 'Nike',
+            rating: 4.6,
+            numReviews: 16,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '5',
+            name: 'Puma Slim Pant',
+            category: 'Pant',
+            image: 'https://amazona.webacademy.pro/images/p5.jpg',
+            price: 65,
+            brand: 'Puma',
+            rating: 4.0,
+            numReviews: 6,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pant',
+            image: 'https://amazona.webacademy.pro/images/p6.jpg',
+            price: 86,
+            brand: 'Adidas',
+            rating: 4.7,
+            numReviews: 25,
+            description: 'high quality product',
+        },
+    ]
+}
+
+export default data
