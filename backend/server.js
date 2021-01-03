fc84b9e2a512417ec32a73d9e0c0ad328f197815
olumayor99 <olumayoor99@gmail.com>
2021-01-03 04:58:27 +0200

    switched from static data to api

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 })
