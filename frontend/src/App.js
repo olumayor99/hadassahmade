240cc7ad3eb841bb161fbe5d348f54609b840422
olumayor99 <olumayoor99@gmail.com>
2021-01-03 02:32:41 +0200

    Installed react router DOM, started adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,45 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Product from './components/Product';
 import data from './data';
 
 function App() {
   return (
-    <div className="grid-container">
+    <BrowserRouter>
+      <div className="grid-container">
 
-            <header className="row">
+        <header className="row">
 
-                <div>
-                    <a className="brand" href="/">HMD</a>
-                </div>
+            <div>
+                <a className="brand" href="/">HMD</a>
+            </div>
 
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign In</a>
-                </div>
+            <div>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
+            </div>
 
-            </header>
+        </header>
 
-            <main>
-                <div className="row center">
-                  {
-                    data.products.map(product => (
-                      <Product key={product._id} product={product}></Product>
-                    ))
-                  }
+        <main>
 
-                </div>
-            </main>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
 
-            <footer className="row center">©2021. All Rights Reserved.</footer>
-        </div>
-     );
+          <div className="row center">
+            {
+              data.products.map(product => (
+                <Product key={product._id} product={product}></Product>
+              ))
+            }
+
+          </div>
+        </main>
+
+        <footer className="row center">©2021. All Rights Reserved.</footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
